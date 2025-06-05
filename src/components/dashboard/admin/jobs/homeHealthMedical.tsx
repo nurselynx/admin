98e74d3f0fd6cc1366f3862fc3a25b88ff4e6ed8
satvinder singh <satvinder.singh@digimantra.com>
2025-06-05 15:01:39 +0530

    changes-done

--- src/components/dashboard/admin/jobs/homeHealthMedical.tsx
+++ src/components/dashboard/admin/jobs/homeHealthMedical.tsx
@@ -67,21 +67,17 @@ export default function HomeHealthMedical({
         ]
       : []),
     // Conditionally add the "Action" column only if isRequests is true
-    ...(isRequests
-      ? [
-          {
-            label: "Action",
-            accessor: "cancel",
-            render: (row: any) => (
-              <CancelReject
-                applicantId={row?.applicantId}
-                handleCancelRequest={handleCancelRequest}
-                status={row?.status}
-              />
-            ),
-          },
-        ]
-      : []),
+    // {
+    //   label: "Action",
+    //   accessor: "cancel",
+    //   render: (row: any) => (
+    //     <CancelReject
+    //       applicantId={row?.applicantId}
+    //       handleCancelRequest={handleCancelRequest}
+    //       status={row?.status}
+    //     />
+    //   ),
+    // },
 
     ...(isAvailable
       ? [
