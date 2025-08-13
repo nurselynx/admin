ba4d1f7bdfd8548d204e0c62b95716b60ff10c5a
satvinder singh <satvinder.singh@digimantra.com>
2025-08-13 15:23:34 +0530

    add-new-field

--- src/components/dashboard/admin/jobs/homeStaffingNeeds.tsx
+++ src/components/dashboard/admin/jobs/homeStaffingNeeds.tsx
@@ -153,25 +153,6 @@ export default function StaffingNeedsMedical({
         ]
       : []),
     {
-      label: "Job Creator",
-      accessor: "requestedBy",
-      render: (row) => decryptData(row?.requestedBy ?? "", secretKey),
-    },
-    {
-      label: "Assigned Professional",
-      accessor: "status",
-      render: (row: any) => (
-        <button
-          type="button"
-          onClick={() => (row?.acceptedBy ? setShowDetails(row) : null)}
-          className=" text-lynx-blue-100"
-        >
-          {row?.acceptedBy ? "User Details" : "N/A"}
-        </button>
-      ),
-    },
-
-    {
       label: "Status",
       accessor: "status",
       render: (row: any) => <StatusBadge status={row?.status ?? 0} />,
